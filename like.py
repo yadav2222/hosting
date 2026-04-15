165869ee466791b16e8800b1836dc7ce6a80db7d
yadav2222 <raoshab67615@gmail.com>
2026-04-15 08:33:56 +0530

    Update like.py

--- like.py
+++ like.py
@@ -13,7 +13,7 @@ BOT_ENABLED = True
 
 # Allowed group IDs and their remaining limits
 ALLOWED_GROUPS = {
-    -5173068786: {"name": "Main Group", "remain": 100, "initial_remain": 100},
+    -1003948351370: {"name": "Main Group", "remain": 100, "initial_remain": 100},
     -1003847529783: {"name": "Friend Group", "remain": 50, "initial_remain": 50},
 }
 
